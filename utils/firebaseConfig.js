416816dec6931207cfa7db3c3e38168c16a5ac21
Vansh Singh <vansh2@ualberta.ca>
2024-08-27 18:46:54 +0530

    initialized firebase and firestore

---
+++ utils/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDstMjqYjUqjxoFr3cTl2zUILhSBbjms-k",
+  authDomain: "review-website--2608.firebaseapp.com",
+  projectId: "review-website--2608",
+  storageBucket: "review-website--2608.appspot.com",
+  messagingSenderId: "440810284958",
+  appId: "1:440810284958:web:267659dcb73b203974988f",
+  measurementId: "G-GD43CVENHL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
