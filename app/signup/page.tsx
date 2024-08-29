2e5c03cf3539cf3285338d7ec4c466a0b6309f11
Vansh Singh <vansh2@ualberta.ca>
2024-08-30 02:32:46 +0530

    Updated Sign Up Logic

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,39 +1,40 @@
 "use client"
 
-import React, { useState } from "react";
+import React, {useState} from 'react'
 import { UserAuth } from "../context/AuthContext";
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
-const SignUpPage = () => {
-  const { googleSignIn } = UserAuth();
-  const [skatingLevel, setSkatingLevel] = useState("beginner");
-  const router = useRouter();
+const SignUp = () => {
+    const { googleSignIn } = UserAuth();
+    const [skatingLevel, setSkatingLevel] = useState("beginner");
+    const router = useRouter();
 
-  const handleSignUp = async () => {
-    try {
-      await googleSignIn(skatingLevel);
-      router.push("/profile"); // Redirect to profile or home after successful sign-up
-    } catch (err) {
-      console.log(err);
-    }
+    const handleSignUp = async () => {
+      try {
+        await googleSignIn(skatingLevel);
+        router.push('/profile');
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    return (
+      <div className="signup-form px-20 h-screen w-auto flex flex-col justify-center items-center">
+      <div className = "flex flex-col gap-24">
+        <label htmlFor="skatingLevel">Select your skating level:</label>
+        <select
+          id="skatingLevel"
+          value={skatingLevel}
+          onChange={(e) => setSkatingLevel(e.target.value)}
+        >
+          <option value="beginner">Beginner</option>
+          <option value="intermediate">Intermediate</option>
+          <option value="professional">Professional</option>
+        </select>
+        <button onClick={handleSignUp}>Sign Up</button>
+      </div>
+      </div>
+    );
   };
 
-  return (
-    <div className="signup-form px-20">
-      <h2>Sign Up</h2>
-      <label htmlFor="skatingLevel">Select your skating level:</label>
-      <select
-        id="skatingLevel"
-        value={skatingLevel}
-        onChange={(e) => setSkatingLevel(e.target.value)}
-      >
-        <option value="beginner">Beginner</option>
-        <option value="intermediate">Intermediate</option>
-        <option value="professional">Professional</option>
-      </select>
-      <button onClick={handleSignUp}>Sign Up</button>
-    </div>
-  );
-};
-
-export default SignUpPage;
+export default SignUp
