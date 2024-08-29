70f6eca2c792732476601979dd7b6c23d1cdb4d5
Vansh Singh <vansh2@ualberta.ca>
2024-08-30 02:29:28 +0530

    Added Form to Add Entity

---
+++ app/addEntity/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+import AddEntity from '../components/AddEntity'
+
+const addEntity = () => {
+  return (
+    <AddEntity/>
+  )
+}
+
+export default addEntity;
