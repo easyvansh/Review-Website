07799fef78c82225a89cad162ebb18230ef69c46
Vansh Singh <vansh2@ualberta.ca>
2024-08-31 03:00:41 +0530

    Updated HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,13 @@
-"use client"
+"use client";
 
-import { useState } from "react";
-import AddReview from "./components/AddReview";
-import ListReviews from "./components/ListReviews";
 import SearchReviews from "./components/SearchReviews";
-import AddEntity from "./components/AddEntity";
-
-
 
 export default function Home() {
-
-
   return (
-        <div className = "h-screen w-screen flex flex-col gap-4 justify-center items-center">
-         <h1>Home</h1>
-         <SearchReviews onSearchResults={{}}/>
-          <a href="/Review"> See REVIEWS</a>
-          <a href="/addEntity"> Add Entity</a>
-          <a href="/entities"> SEE Entities</a>
-          
-         
-         {/* 
-         //Click here to see list of reviews
-          <a href = 
-         //Click here to add review
-         <AddReview/>
-         
-         <ListReviews/> */}
-        </div>
+    <div className="h-screen w-screen flex flex-col gap-2 justify-center items-center">
+      <h1 className="text-xl font-mono">Welcome to Skate Review!!</h1>
+      <h2 className="text-3xl font-mono">An Inventory for All Things Skating</h2>
+      <SearchReviews />
+    </div>
   );
 }
